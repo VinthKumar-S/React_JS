db51b9d82393a2b2501fdb37f9399702dd2f2549
Vinithkumar-S <vk3228433@gmail.com>
2024-08-08 22:25:04 +0530

    add

---
+++ prog2/src/App.js
@@ -0,0 +1,16 @@
+import { Header } from "./file";
+import { Footer } from "./file";
+import { Main } from "./file";
+
+
+function App() {
+  return (
+      <div>
+          <Header></Header>
+          <Main></Main>
+          <Footer></Footer>
+      </div>
+  );
+}
+
+export default App;
---
+++ prog2/src/file.js
@@ -0,0 +1,34 @@
+import './style.css';
+
+function Header(){
+    return (
+        <div class="header">
+            <img
+                src="https://cdn.kinocheck.com/i/d3mx9y2anl.jpg"
+                class="img-fluid rounded-top"
+                alt=""
+            />
+            
+        </div>
+    );
+}
+
+function Main(){
+    return (
+        <div class="main">
+            <p >
+                Deadpool has dreamed of becoming an Avenger for years - and now the shady TVA agent Mr. Paradox offers him the chance to switch to the sacred timeline and become part of the MCU. But the cheeky mercenary has to make a big sacrifice: His world will be wiped out. Because Deadpool won't let this happen, Mr. Paradox sends him and a washed-up version of Wolverine recruited by Deadpool into the Void - the great dumping ground of all universes. There, the sinister Cassandra Nova has established a regime of terror. Deadpool knows that if he wants to save his world, all his friends and his great love Vanessa, he has to get past her and stop Mr. Paradox. But first he has to convince the embittered Wolverine to help him...
+            </p>
+        </div>
+    );
+}
+
+function Footer(){
+    <div class="footer">
+        <img
+            src="https://cdn.kinocheck.com/i/d3mx9y2anl.jpg"
+        />
+    </div>
+}
+
+export{Header,Main,Footer};
